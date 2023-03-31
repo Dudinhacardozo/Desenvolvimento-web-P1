cf523feb5ebff344c96e59a0c60da993b8c2706d
= <=>
2023-03-31 11:49:45 -0300

    começo do projeto 1

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+const texto = ref('')
+</script>
+
+<template>
+  <div>
+        <input type="text" v-model="texto" />
+        <p>Texto digitado: {{ texto }}</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
